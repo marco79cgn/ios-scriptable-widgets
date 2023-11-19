60acf3e762ef1b23b7b503e45c28f659b49dd062
marco79cgn <marco79cgn@gmail.com>
2023-11-20 00:10:29 +0100

    temperature is more precisely

--- smart/smart-one-info-small.js
+++ smart/smart-one-info-small.js
@@ -215,9 +215,9 @@ async function createWidget() {
 
     distanceStack.addSpacer(1)
     let temperature = carData.data.vehicleStatus.additionalVehicleStatus.climateStatus.interiorTemp;
-    let totalDistaneNo = distanceStack.addText(Math.round(temperature) + '°C');
-    totalDistaneNo.font = Font.semiboldSystemFont(11);
-    totalDistaneNo.textColor = textColor;
+    let temperatureNo = distanceStack.addText(Number.parseFloat(temperature).toFixed(1) + '°C');
+    temperatureNo.font = Font.semiboldSystemFont(11);
+    temperatureNo.textColor = textColor;
 
     detailsStack.addSpacer();
 
